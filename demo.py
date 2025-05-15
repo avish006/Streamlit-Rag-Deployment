af1e27127a2ea5fd2d9fddd23257b002bff2650d
avish006 <167643918+avish006@users.noreply.github.com>
2025-05-16 03:48:52 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import base64
 
-def display_pdf(file_bytes):
-    base64_pdf = base64.b64encode(file_bytes).decode('utf-8')
-    return f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf">'
+st.set_page_config(page_title="PDF Viewer", layout="wide")
 
-st.set_page_config(layout="wide", page_title="PDF Viewer")
+def display_pdf(pdf_bytes):
+    base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
+    pdf_display = f'''
+        <iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="700" type="application/pdf"></iframe>
+    '''
+    return pdf_display
 
-st.sidebar.header("Upload PDF")
-uploaded_file = st.sidebar.file_uploader("Choose a PDF", type=["pdf"])
+st.title("📄 PDF Viewer")
+
+uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
 if uploaded_file:
-    pdf_bytes = uploaded_file.getvalue()
-    st.session_state.uploaded_pdf = pdf_bytes
-
-if 'uploaded_pdf' in st.session_state:
-    st.markdown(display_pdf(st.session_state.uploaded_pdf), unsafe_allow_html=True)
+    pdf_bytes = uploaded_file.read()
+    st.markdown(display_pdf(pdf_bytes), unsafe_allow_html=True)
 else:
-    st.info("Upload a PDF to view it here.")
+    st.info("Please upload a PDF to view it here.")
